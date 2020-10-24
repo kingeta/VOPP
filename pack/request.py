b7ce6102b9da2d3a0907821f97113c9eea937576
Adam Sroka <adamsroka286@gmail.com>
2020-10-24 14:36:52 +0100

    Cleanup request

--- pack/request.py
+++ pack/request.py
@@ -1,6 +1,8 @@
 import requests
 
 
+url = 'https://api.paccurate.io/'
+
 payload = """{
       "itemSets": [{
       "refId": 0,
@@ -10,8 +12,9 @@ payload = """{
   "boxTypeSets": ["fedex"]
 }"""
 
-url = 'https://api.paccurate.io/'
+
 headers = {'Authorization': 'apikey PVjuMKCNFDZcPKdSJgeH_zun3r2ZAAiU-cXk0TClmc-zEUCogYBbvbepucZV8T3z'}
+
 r = requests.post(url, data=payload, headers=headers)
 
 
