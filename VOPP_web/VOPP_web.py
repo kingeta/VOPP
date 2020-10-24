bdce509ddbeabd7c45288aa071e21c1858bc179d
kingeta <herbybowden@gmail.com>
2020-10-24 16:47:22 -0400

    fixed indexing database with quantities

--- VOPP_web/VOPP_web.py
+++ VOPP_web/VOPP_web.py
@@ -10,13 +10,18 @@ app = Flask(__name__)
 app.config['Mongo_URI'] = mongo_uri
 mongo = PyMongo(app, uri = mongo_uri)
 
+def format_input(input_order):
+    formatted = input_order.split("\n")
+    for i in range(len(formatted)):
+        formatted[i] = list(map(int, formatted[i].split(" ")))
+    return formatted
+
 @app.route('/test')
 def test():
     inputs = "0 2\n1 1"
-    items = inputs.split("\n")
-    for i in range(len(items)):
-        items[i] = items[i].split(" ")
+    items = format_input(inputs)
     obj_list = []
+    app.logger.debug(obj_list)
     for item_id in items:
         i_params = mongo.db['inventory'].find()[item_id[0]]
         obj_list.append(warehouse.ItemSet((i_params['x'],i_params['y'],i_params['z']),i_params['weight'],item_id[0], item_id[1]))
