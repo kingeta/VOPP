49cb16a255a25ee3b32de2e8b999096a376d72af
Egle Augustaityte <ea486@cam.ac.uk>
2020-10-24 18:54:36 +0100

    different packages in one box

---
+++ pack/demos/Couple_different_packages.py
@@ -0,0 +1,42 @@
+from request import request_function
+
+payload = """{
+  "itemSets": [
+    {
+      "refId": 0,
+      "color": "tomato",
+      "weight": 0,
+      "dimensions": {
+        "x": 5,
+        "y": 6,
+        "z": 4
+      },
+      "quantity": 1
+    },
+    {
+      "refId": 1,
+      "color": "cornflowerblue",
+      "weight": 0,
+      "dimensions": {
+        "x": 2.5,
+        "y": 3,
+        "z": 2
+      },
+      "quantity": 4
+    }
+    ],
+    "boxTypes": [
+    {
+      "weightMax": 150,
+      "name": "5x6x8",
+      "dimensions": {
+        "x": 5,
+        "y": 6,
+        "z": 8
+      }
+    }
+    ]
+    }"""
+
+r=request_function(payload)
+print (r.text)
