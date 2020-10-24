0833d88e0d0ba0fedef37a3bf3f80025bb9fe9a1
Egle Augustaityte <ea486@cam.ac.uk>
2020-10-24 15:01:15 +0100

    add request file

---
+++ pack/request_Egle.py
@@ -0,0 +1,21 @@
+import requests
+
+
+url = 'https://api.paccurate.io/'
+
+payload = """{
+      "itemSets": [{
+      "refId": 0,
+      "dimensions": {"x": 5.5, "y": 6, "z": 6},
+      "quantity": 3
+  }],
+  "boxTypeSets": ["fedex"]
+}"""
+
+
+headers = {'Authorization': 'apikey PVjuMKCNFDZcPKdSJgeH_zun3r2ZAAiU-cXk0TClmc-zEUCogYBbvbepucZV8T3z'}
+
+r = requests.post(url, data=payload, headers=headers)
+
+print(r.text)
+
