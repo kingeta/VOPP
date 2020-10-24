7875d3fca1c6c53497bc50c7b506985d13b33828
Adam Sroka <adamsroka286@gmail.com>
2020-10-24 18:36:31 +0100

    Add package getter Packer class template

--- pack/package_getter.py
+++ pack/package_getter.py
@@ -1,7 +1,19 @@
 """Send Items and Box to paccurate API to return Packages"""
+import requests
 from .. import warehouse
 
 
 class Packer:
     """Returning list of packages"""
-    pass
+    def __init__(self):
+        self.payload = None
+        self.headers = None
+
+    def set_api_key(self, key):
+        self.headers = None  # TODO
+
+    def get_payload(self, items, box):
+        pass
+
+    def get_package(self, payload):
+        pass
