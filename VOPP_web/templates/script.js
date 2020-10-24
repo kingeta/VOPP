5576dd7082aa2090c00ff182593587e92f2a2e7f
dobm <david.obm@gmail.com>
2020-10-24 21:45:24 +0100

    changes to shipping input

--- VOPP_web/templates/script.js
+++ VOPP_web/templates/script.js
@@ -1,27 +1,25 @@
 var ENTER_KEY = 13;
 
 $(document).on('keypress', function(e) {
-    console.log("javaisup");
   var code = e.keyCode || e.which
   if (code == ENTER_KEY) {
     var itemString = document.getElementById('bar_input').value;
-    console.log("hello")
- //   $("#bar_submit").trigger("click");
-
-    check(itemString)
+    console.log(itemString);
+    document.getElementById('bar_input').value = '';
+    check(itemString) 
     
   }
 });
 
 function check(itemString) {
-    console.log("ere");
     console.log(itemString)
     if(document.getElementById(itemString)!== null){
-        console.log("hello!!")
         document.getElementById(itemString).checked = true;
-    }
-    else{
+        document.getElementById(itemString).id = "checked";
 
     }
+    else{
+    }
+
     return true;
 }
