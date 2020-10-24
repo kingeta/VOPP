d1f33a22f45bf6c5adbfd3d9a27c867b5d2ee076
kingeta <herbybowden@gmail.com>
2020-10-24 12:45:59 -0400

    Adding some of Louis' code

--- warehouse.py
+++ warehouse.py
@@ -6,13 +6,16 @@ class ItemSet():
     
     The dimensions x, y, z are the dimensions of a single item.
     The weight is a weight of a single member"""
-    def __init__(self, x, y, z, weight, ref_id, quantity):
+    def __init__(self, x, y, z, weight, ref_id, quantity = 1):
         self.x = x
         self.y = y
         self.z = z
         self.weight = weight
         self.ref_id = ref_id
         self.quantity = quantity
+    
+    def __str__(self):
+        return f"({self.x}, {self.y}, {self.z}), {self.weight}"
 
 
 class Box():
