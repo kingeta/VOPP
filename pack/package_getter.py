b9e16435a3f1462b1821d6ffbbdae0062d03cb63
Adam Sroka <adamsroka286@gmail.com>
2020-10-25 01:19:10 +0000

    Fix get_payload into set_payload method in Packer

--- pack/package_getter.py
+++ pack/package_getter.py
@@ -1,6 +1,6 @@
-"""Send Items and Box to paccurate API to return Packages"""
-import requests, json
-# from .. import warehouse
+"""Module for sending Items and Boxes to Paccurate API to return Packages"""
+import requests
+import json
 
 
 class Packer:
@@ -15,7 +15,7 @@ class Packer:
         api_key_value = f'apikey {key}'
         self.headers = {'Authorization': api_key_value}
 
-    def get_payload(self, items, box):
+    def get_payload(self, items, box):  # Deprecated TODO Delete
         concatenate = '''{"itemSets": ['''
         for item in items:
             item_string = '''{
@@ -35,6 +35,31 @@ class Packer:
         }''' % (str(box.max_weight), str(box.name), str(box.dimensions[0]), str(box.dimensions[1]), str(box.dimensions[2]))
         concatenate += box_string
         self.payload = json.dumps(concatenate)
+    
+    def set_payload(self, items, boxes):
+        """Items is a list of class ItemSet objects and boxes is a list of class Box objects"""
+        itemsets = list()
+        for itemset in items:
+            itemset_dimensions = dict(x=itemset.dimensions[0],
+                                      y=itemset.dimensions[1],
+                                      z=itemset.dimensions[2])
+            itemsets.append(dict(refId=itemset.ref_id,
+                                 weight=itemset.weight,
+                                 dimensions=itemset_dimensions,
+                                 quantity=itemset.quantity))
+        boxtypes = list()
+        for box in boxes:
+            box_dimensions = dict(x=box.dimensions[0],
+                                  y=box.dimensions[1],
+                                  z=box.dimensions[2])
+            boxtypes.append(dict(weightMax=box.max_weight,
+                                 name=box.name,
+                                 dimensions=box_dimensions))
+
+        payload = dict(itemSets=itemsets,
+                       boxTypes=boxtypes)
+        self.payload = json.dumps(payload)
+
 
     def get_response(self):
         if self.url is None:
@@ -48,4 +73,5 @@ class Packer:
     def get_package(self):
         if self.response is None:
             raise ValueError('There is no response from the Paccurate API')
-        
+        # TODO
+        # return json.loads(self.response.text)
