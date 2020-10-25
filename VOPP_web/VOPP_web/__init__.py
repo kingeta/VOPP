d0485f3746e45ca5fb3b713d6dbe94f51834c355
kingeta <herbybowden@gmail.com>
2020-10-25 09:12:40 -0400

    receiving and shipping getting data from database

--- VOPP_web/VOPP_web/__init__.py
+++ VOPP_web/VOPP_web/__init__.py
@@ -18,6 +18,7 @@ app.config['Mongo_URI'] = mongo_uri
 #application = app.app
 mongo = PyMongo(app, uri = mongo_uri)
 obj_list = []
+list_item_ids = []
 
 def format_input(input_order):
     formatted = input_order.split("\n")
@@ -25,22 +26,29 @@ def format_input(input_order):
         formatted[i] = list(map(int, formatted[i].split(" ")))
     return formatted
 
+# def format_input_2(input_order):
+#     formatted = input_order.split('\n')
+#     return {int(a): int(b) for a, b in map(lambda t: t.split(' '), formatted)}
+
 @app.route('/test')
 def test():
     obj_list.clear()
-    inputs = "0 1\n1 5"
+    inputs = "9780007466061 2\n5060181370243 1"
+    # inputs = "0 2\n1 2"
     items = format_input(inputs)
-    app.logger.debug(obj_list)
-    for item_id in items:
-        i_params = mongo.db['inventory'].find()[item_id[0]]
-        obj_list.append(ItemSet((i_params['x'],i_params['y'],i_params['z']),i_params['weight'],item_id[0], item_id[1]))
-    app.logger.debug(obj_list)
+    # app.logger.debug('items', items)
+    # app.logger.debug(obj_list)
+    
+    for item, quantity in items:
+        i_params = mongo.db['storage'].find({'item': item})[0] 
+        obj_list.append(ItemSet((i_params['x']/5,i_params['y']/5,i_params['z']/5),i_params['weight'], item, quantity, i_params['color']))
+    
     #return ', '.join(map(lambda t: str(t.x), obj_list))
     return "\r\n".join(map(str, obj_list))
 
 @app.route('/')
 def input():
-    return render_template('input.jinja', items = )
+    return render_template('input.jinja')
     
 
 @app.route('/add', methods = ['POST'])
@@ -52,7 +60,13 @@ def input_read():
 
 @app.route('/shipping')
 def shipping():
-    return render_template('worker.jinja')
+    test()
+    list_item_ids.clear()
+    for it in obj_list:
+        for _ in range(it.quantity):
+            list_item_ids.append(str(it.ref_id))
+    app.logger.debug(list_item_ids)
+    return render_template('worker.jinja', items=list_item_ids) # list of ids as strings
 
 @app.route('/shipping/addCheckBox', methods = ['POST'])
 def checkbox_input():
@@ -73,7 +87,7 @@ def receiving():
     # items = [ItemSet((5, 6, 4), 2, 0, 4)]
     test()
     items = obj_list
-    app.logger.debug(items)
+    # app.logger.debug(items)
 
     # Specify types of boxes to be used — only one type in this case
     boxes = [Box("5x6x8", (5, 6, 8), 150)]
@@ -84,7 +98,5 @@ def receiving():
     packages = WarehousePacker.pack(items, boxes)
 
     # app.logger.debug(packages[0].image)
-
-
 
     return render_template('receiver.jinja', images = [p.image for p in packages])
