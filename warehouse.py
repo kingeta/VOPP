29fb32f1de8111e4be695d48a6a4fe1507b7fbdd
Adam Sroka <adamsroka286@gmail.com>
2020-10-25 03:56:08 +0000

    Add string representations and update docstrings

--- warehouse.py
+++ warehouse.py
@@ -13,23 +13,29 @@ class ItemSet:
         self.quantity = quantity
     
     def __str__(self):
-        return f"{self.dimensions},{self.weight}"
+        return f"An ItemSet of {self.quantity} individual items of size {self.dimensions}, individual weight {self.weight}, id {self.ref_id}"
 
 
 class Box:
-    """ dimensions is a tuple of (x, y, z)"""
+    """A Box dimensions is a tuple of (x, y, z)"""
     def __init__(self, name, dimensions, max_weight):
         self.name = name
         self.dimensions = dimensions
         self.max_weight = max_weight
+    
+    def __str__(self):
+        return f'A {self.name} Box of size {self.dimensions} and maximum weight {self.max_weight}'
 
 
 class Package:
-    """A package corresponds to one box.
-    items of class ItemSet of quantity 1, packed in a box of class Box
-    items is a list of tuples ordered by the order of packages being put in the box
+    """A Package corresponds to one Box that contains
+    items of class ItemSet of quantity 1.
+    items_and_locations is a list of tuples ordered by the order of packages being put in the box
     each tuple contains a ItemSet class and a """
-    def __init__(self, items, box, image):
-        self.items = items
+    def __init__(self, items_and_locations, box, image):
+        self.items_and_locations = items_and_locations
         self.box = box
         self.image = image
+
+    def __str__(self):
+        return f'Package of Box {self.box} containing ItemSets at locations {self.items_and_locations}'
